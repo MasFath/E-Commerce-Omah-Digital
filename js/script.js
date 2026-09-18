67c42d9b9191211779f20b1277df95f6ee8d5046
Mas Fath <ibnumaftuhin@gmail.com>
2026-09-18 23:04:47 +0700

    Move Landing Page to root

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 //Toggle class active
 const navbarNav =document.querySelector('.navbar-nav');
 // ketika hamburger menu diklik
-document.querySelector('#hamburger-menu').onclick=()=>{
+document.querySelector('#hamburger-menu').onclick=(f)=>{
     navbarNav.classList.toggle('active');
+    f.preventDefault();
 };
-// klik diluar sidebar untuk menghilangkan nav
-const hamburger=document.querySelector('#hamburger-menu');
+// toggle search
+const searchForm = document.querySelector('.search-form');
+const searchBox =document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e)=>{
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+
+};
+// klik diluar element
+const hm=document.querySelector('#hamburger-menu');
+const sb=document.querySelector('#search-button');
 document.addEventListener('click', function(e){
-    if(!hamburger.contains(e.target)&&!navbarNav.contains(e.target)){
+    if(!hm.contains(e.target)&&!navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
+    }
+
+    if(!sb.contains(e.target)&&!searchForm.contains(e.target)){
+        searchForm.classList.remove('active');
     }
 })
